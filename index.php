cc7b5a50f5a2d67b9270ec1eb3fd5e9a8859527e
小明 <86601505+NekohaYuchi@users.noreply.github.com>
2025-06-09 21:09:18 +0800

    Add files via upload
    fix the appearance of the login background and add signup button

--- index.php
+++ index.php
@@ -55,43 +55,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	<body class="img js-fullheight" style="background-image: url(images/bg.jpg);">
 	<section class="ftco-section">
 		<!-- 登入格 -->
-		<div class="container">
+		<div class="container" style="max-width: 400px;">
 			<div class="row justify-content-center">
-			</div>
-			<div class="row justify-content-center">
-				<div class="col-md-6 col-lg-4">
+				<div class="col-md-11 col-lg-9">
 					<div class="login-wrap p-0">
-		      	<h3 class="mb-4 text-center">Have an account?</h3>
-		      	<form action="index.php" method="post" class="signin-form">
-		      		<div class="form-group">
-		      			<input type="text" name="Username" class="form-control" placeholder="Username" required>
-		      		</div>
-	            <div class="form-group">
-	              <input id="password-field" type="password" name="Password" class="form-control" placeholder="Password" required>
-	              <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-	            </div>
-	            <div class="form-group">
-	            	<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
-	            </div>
-	            <div class="form-group d-md-flex">
-	            	<div class="w-50">
-		            	<label class="checkbox-wrap checkbox-primary">Remember Me
-									  <input type="checkbox" checked>
-									  <span class="checkmark"></span>
+						<h3 class="mb-4 text-center">Have an account?</h3>
+						<form action="index.php" method="post" class="signin-form">
+							<div class="form-group">
+								<input type="text" name="Username" class="form-control" placeholder="Username" required>
+							</div>
+							<div class="form-group">
+								<input id="password-field" type="password" name="Password" class="form-control" placeholder="Password" required>
+								<span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
+							</div>
+							<div class="form-group">
+								<button type="submit" class="form-control btn btn-primary submit px-3">Sign In</button>
+							</div>
+							<div class="form-group d-md-flex">
+								<div class="w-50">
+									<label class="checkbox-wrap checkbox-primary">Remember Me
+										<input type="checkbox" checked>
+										<span class="checkmark"></span>
 									</label>
 								</div>
 								<div class="w-50 text-md-right">
 									<a href="#" style="color: #fff">Forgot Password</a>
 								</div>
-	            </div>
-	          </form>
-		      </div>
+								
+	            			</div>
+							<div class="text-center mt-3">
+								<p style="color: white;">Don't have an account? &nbsp;&nbsp;&nbsp;<a href="signup.php" style="color: 	#fff;">Sign up here</a></p>
+							</div>
+	          			</form>
+		      		</div>
 				</div>
 			</div>
 		</div>
 	</section>
 
-	<script src="js/jquery.min.js"></script>
+  <script src="js/jquery.min.js"></script>
   <script src="js/popper.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/main.js"></script>
